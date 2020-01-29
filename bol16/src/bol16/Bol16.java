8ba7282217b4e034bceb884c4717d9a6e77c6198
ipampillonvieitez <ipampillonvieitez@danielcastelao.org>
2020-01-29 10:47:11 +0100

    bol16

---
+++ bol16/src/bol16/Bol16.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bol16;
+
+/**
+ *
+ * @author ipampillonvieitez
+ */
+public class Bol16 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+             Canario canario=new Canario();
+        Persoa tenor=new Persoa();
+        tenor.Cantar();
+        canario.Cantar();
+    
+    }
+    
+}
---
+++ bol16/src/bol16/Galo.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bol16;
+
+/**
+ *
+ * @author ipampillonvieitez
+ */
+public class Galo extends IPodeCantar{
+      @Override
+    public void Cantar() {
+        System.out.println("Kikiriki");
+    }
+}
